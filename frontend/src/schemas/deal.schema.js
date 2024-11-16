076e4e0d1f9501a476596dc31bd1051f842896af
Nick Thiru <nickthiru@gmail.com>
2024-11-17 00:50:40 +0530

    Update deal schema and related tests and assets for backend and frontend
    - **Backend**:
      - **package-lock.json**: Updated dependencies to ensure compatibility with recent changes.
      - **package.json**: Updated dependencies to ensure compatibility with recent changes.
      - **schemas/deal.schema.js**: Adjusted schema definitions for deal validation, including new validation rules for deal expiration date and file type checks.
      - **tests/data/deal-logo.png**: Added/updated test asset for deal logo.
      - **tests/domains/deal/api-endpoints/index.test.js**: Modified tests to reflect changes in schema validations and data structure.
      - **tests/domains/deal/workflows/add-deal/event.json**: Updated event payload to align with new schema structure.
      - **tests/domains/deal/workflows/add-deal/lambda-handler.test.js**: Adjusted tests to validate the new schema and handle additional validation scenarios.
      - **tests/postman-collections/Super Deals.json**: Updated Postman collection to include new test cases for deal validation and submission.
    
    - **Frontend**:
      - **src/schemas/deal.schema copy.js**: Copied the updated schema for redundancy and potential future updates.
      - **src/schemas/deal.schema.js**: Enhanced schema definitions to include stricter validation rules for deal properties, ensuring better data integrity.
    
    These updates enhance the robustness of the deal validation process across both backend and frontend components, ensuring consistent data validation and improved test coverage.

--- frontend/src/schemas/deal.schema.js
+++ frontend/src/schemas/deal.schema.js
@@ -27,19 +27,38 @@ const categoryEnum = [
  * Common schema object.
  * @type {object}
  */
+// const commonSchemaObject = {
+//   merchantId: zfd.text(z.string().min(1, 'Merchant ID is required')),
+//   title: zfd.text(z.string().min(1, 'Title is required').max(255, 'Title must be 255 characters or less')),
+//   originalPrice: zfd.text(z.string().refine(val => val.length > 0, { message: 'Original Price is required' }).transform(val => parseFloat(val)).refine(val => !isNaN(val) && val > 0, { message: 'Original Price must be more than 0' })),
+//   discount: zfd.text(z.string().refine(val => val.length > 0, { message: 'Discount is required' }).transform(val => parseFloat(val)).refine(val => !isNaN(val) && val > 0 && val <= 100, { message: 'Discount must be between 0 and 100' })),
+//   logo: zfd.file(z.any()).refine(val => val !== undefined && val !== null, { message: 'Logo is required' }).refine(file => {
+//     if (file) {
+//       const fileType = file.name.split('.').pop().toLowerCase();
+//       return allowedFileTypes.includes(fileType);
+//     }
+//     return true;
+//   }, 'Invalid file type'),
+//   category: zfd.text(z.enum(categoryEnum, 'Category is required')),
+//   expiration: zfd.text(z.string().min(1, 'Expiration is required').refine((val) => !isNaN(Date.parse(val)), 'Expiration must be a valid date')),
+// };
 const commonSchemaObject = {
   merchantId: zfd.text(z.string().min(1, 'Merchant ID is required')),
-  title: zfd.text(z.string().min(1, 'Required').max(255, 'Title must be 255 characters or less')),
-  originalPrice: zfd.text(z.string().refine(val => val.length > 0, { message: 'Required' }).transform(val => parseFloat(val)).refine(val => !isNaN(val) && val > 0, { message: 'Original Price must be more than 0' })),
-  discount: zfd.text(z.string().refine(val => val.length > 0, { message: 'Required' }).transform(val => parseFloat(val)).refine(val => !isNaN(val) && val > 0 && val <= 100, { message: 'Discount must be between 0 and 100' })),
-  logo: zfd.file(z.any()).refine(val => val !== undefined && val !== null, { message: 'Required' }).refine(file => {
-    if (file) {
-      const fileType = file.name.split('.').pop().toLowerCase();
-      return allowedFileTypes.includes(fileType);
-    }
-    return true;
-  }, 'Invalid file type'),
-  category: zfd.text(z.enum(categoryEnum, 'Required')),
+  title: zfd.text(z.string().min(1, 'Title is required').max(255, 'Title must be 255 characters or less')),
+  originalPrice: zfd.numeric(z.number().min(1, 'Original Price is required').positive('Original Price must be a positive number')),
+  discount: zfd.numeric(z.number().min(0, 'Discount is required').max(100, 'Discount must be between 0 and 100')),
+  logo: zfd.file(
+    z.object({
+      filename: z.string().min(1, "Filename is required"),
+      contentType: z.string().refine((type) => {
+        const fileType = type.split('/').pop().toLowerCase();
+        return allowedFileTypes.includes(fileType);
+      }, "Invalid file type"),
+      data: z.instanceof(File, "File data must be a file")
+    })
+  ),
+  category: zfd.text(z.enum(categoryEnum, 'Category is required')),
+  expiration: zfd.text(z.string().min(1, 'Expiration is required').refine((val) => !isNaN(Date.parse(val)), 'Expiration must be a valid date')),
 };
 
 /**
@@ -55,21 +74,33 @@ const getDealSchema = () => {
 
   return zfd.formData({
     ...commonSchemaObject,
-    expiration: zfd.text(z.string().refine(val => val.length > 0, { message: 'Required' }).refine(val => {
+    expiration: zfd.text(z.string().refine(val => val.length > 0, { message: 'Expiration is required' }).refine(val => {
       const parsedDate = Date.parse(val);
       return !isNaN(parsedDate) && new Date(parsedDate) >= sevenDaysFromToday;
     }, 'Expiration must be seven days from today or later')),
   });
 };
+// const getDealSchema = () => {
+//   const today = new Date();
+//   today.setHours(0, 0, 0, 0);
+
+//   const sevenDaysFromToday = new Date(today);
+//   sevenDaysFromToday.setDate(today.getDate() + 7);
+
+//   return zfd.formData({
+//     ...commonSchemaObject,
+//     expiration: zfd.text(z.string().min(1, 'Expiration is required').refine(val => {
+//       const parsedDate = Date.parse(val);
+//       return !isNaN(parsedDate) && new Date(parsedDate) >= sevenDaysFromToday;
+//     }, 'Expiration must be seven days from today or later')),
+//   });
+// };
 
 /**
  * Static schema for API Gateway validation.
  * This schema doesn't include the dynamic check for expiration date.
  */
-const staticDealSchema = zfd.formData({
-  ...commonSchemaObject,
-  expiration: zfd.text(z.string().refine(val => val.length > 0, { message: 'Required' }).refine((val) => !isNaN(Date.parse(val)), 'Expiration must be a valid date')),
-});
+const staticDealSchema = zfd.formData(commonSchemaObject);
 
 /**
  * JSON schema for the deal.
